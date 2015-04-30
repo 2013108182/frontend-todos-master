a43b8bd24a3fa93dafbecd159c3c1cbdda4d3006
2013108182 <jisoo.9955@gmail.com>
2015-04-30 14:54:37 +0900

    0430-1

--- 03/search.js
+++ 03/search.js
@@ -2,26 +2,28 @@
 
 (function(){
 
-var boxOfficeAPI;
+var API;
 
-searchButton.onclick = function(){
-	var searchText = searchBar.value;
-	searchBar.value = '';
-	boxOfficeAPI = 'http://apis.daum.net/search/web?apikey=8cd181d1334b0f9917aea2f24f2c2979&q='+searchText+'&output=json';
+
+searchButton.addEventListener('click', function(){
+	var searchValue = searchWindow.value;
+	searchWindow.value = '';
+	API = 'http://apis.daum.net/search/board?apikey=3dcee193ff3447ade308264b8f36fc07&q='+searchValue+'&output=json';
 	jsonp();
-};
+
+});
 
 
 function jsonp(){
 
 var listTemplate = $('listTemplate').innerHTML;
-var searchBar = $('searchBar');
+var searchWindow = $('searchWindow');
 var searchButton = $('searchButton');
 
 
 console.log(listTemplate);
-getJSON(boxOfficeAPI, function(boxOfficeData){
-  console.log(boxOfficeData);
+getJSON(API, function(Data){
+  console.log(Data);
 
   // 템플릿 가져와서 parse
   var html = tmpl(listTemplate, {list:boxOfficeData.channel.item});
